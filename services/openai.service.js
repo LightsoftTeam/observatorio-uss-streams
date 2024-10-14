57ac9d78458befa40710f2963c637a9ba5b39ea9
José Martinez <proveedor-jmartinez@inteligogroup.com>
2024-10-14 11:22:22 -0500

    move model gpt to env variable

--- services/openai.service.js
+++ services/openai.service.js
@@ -4,6 +4,7 @@ import OpenAI from 'openai';
 dotenv.config();
 
 const SYSTEM_EXPERT_PROMPT = 'Eres un investigador español experimentado, experto en interpretar y responder preguntas basadas en las fuentes proporcionadas. Utilizando el contexto proporcionado entre las etiquetas <context></context>, genera una respuesta concisa para una pregunta rodeada con las etiquetas <question></question>. Debes usar únicamente información del contexto. Usa un tono imparcial y periodístico. No repitas texto. Si no hay nada en el contexto relevante para la pregunta en cuestión, simplemente di "No lo sé". No intentes inventar una respuesta. Cualquier cosa entre los siguientes bloques html context se recupera de un banco de conocimientos, no es parte de la conversación con el usuario.';
+const OPENAI_MODEL = process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo-16k';
 
 export class OpenaiService {
     static openai = new OpenAI({
@@ -12,7 +13,7 @@ export class OpenaiService {
 
     static getCompletion(messages) {
         return OpenaiService.openai.chat.completions.create({
-            model: "gpt-3.5-turbo-16k",
+            model: OPENAI_MODEL,
             messages: [
                 { role: "system", content: "You are a helpful assistant." },
                 ...messages
@@ -26,7 +27,7 @@ export class OpenaiService {
         question,
     }) {
         return OpenaiService.openai.chat.completions.create({
-            model: 'gpt-3.5-turbo-0125',
+            model: OPENAI_MODEL,
             stream: true,
             messages: [
                 {
