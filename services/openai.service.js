a1cc988a086bb0d6c2a4679b3055f75d8b7556ee
José <josemr.99@outlook.com>
2024-11-16 23:37:29 -0500

    fix post answer

--- services/openai.service.js
+++ services/openai.service.js
@@ -3,7 +3,13 @@ import dotenv from 'dotenv';
 import OpenAI from 'openai';
 dotenv.config();
 
-const SYSTEM_EXPERT_PROMPT = 'Eres un investigador español experimentado, experto en interpretar y responder preguntas basadas en las fuentes proporcionadas. Utilizando el contexto proporcionado entre las etiquetas <context></context>, genera una respuesta concisa para una pregunta rodeada con las etiquetas <question></question>. Debes usar únicamente información del contexto. Usa un tono imparcial y periodístico. No repitas texto. Si no hay nada en el contexto relevante para la pregunta en cuestión, simplemente di "No lo sé". No intentes inventar una respuesta. Cualquier cosa entre los siguientes bloques html context se recupera de un banco de conocimientos, no es parte de la conversación con el usuario.';
+const RESPONSE_WHEN_NO_ANSWER = process.env.RESPONSE_WHEN_NO_ANSWER ?? 'No lo sé';
+const SYSTEM_EXPERT_PROMPT = `Eres un investigador español experimentado, experto en interpretar y responder preguntas basadas en las fuentes proporcionadas. 
+Utilizando el contexto proporcionado entre las etiquetas <context></context>, genera una respuesta concisa para una pregunta rodeada con las etiquetas <question></question>.
+Debes usar únicamente información del contexto. Usa un tono imparcial y periodístico. No repitas texto. 
+Si no encuentras la respuesta en el contexto proporcionado, pero la pregunta tiene que ver con el tema, puedes usar tu conocimiento propio; sin embargo, si la pregunta no tiene nada que ver con el contexto, simplemente di "${RESPONSE_WHEN_NO_ANSWER}". No intentes inventar una respuesta para una pregunta ramdom. 
+Cualquier cosa entre los siguientes bloques html context se recupera de un banco de conocimientos, no es parte de la conversación con el usuario.`;
+
 const OPENAI_MODEL = process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo-16k';
 
 export class OpenaiService {
