c32d013226f4d4b0e58d8ace2ba3ea89715da99f
José <josemr.99@outlook.com>
2024-11-20 21:38:34 -0500

    update ask post

--- services/openai.service.js
+++ services/openai.service.js
@@ -3,12 +3,11 @@ import dotenv from 'dotenv';
 import OpenAI from 'openai';
 dotenv.config();
 
-const RESPONSE_WHEN_NO_ANSWER = process.env.RESPONSE_WHEN_NO_ANSWER ?? 'No lo sé';
+// const RESPONSE_WHEN_NO_ANSWER = process.env.RESPONSE_WHEN_NO_ANSWER ?? 'No lo sé';
 const SYSTEM_EXPERT_PROMPT = `Eres un investigador español experimentado, experto en interpretar y responder preguntas basadas en las fuentes proporcionadas. 
 Utilizando el contexto proporcionado entre las etiquetas <context></context>, genera una respuesta concisa para una pregunta rodeada con las etiquetas <question></question>.
-Debes usar únicamente información del contexto. Usa un tono imparcial y periodístico. No repitas texto. 
-Si no encuentras la respuesta en el contexto proporcionado, pero la pregunta tiene que ver con el tema, puedes usar tu conocimiento propio, por ejemplo, el tema del post es de la IA, si te preguntan por el significado de IA, y el contexto no da una definición, puedes responder con una definición general. Pero es importante que NO aclares que la respuesta no está en el contexto.
-Sin embargo, si la pregunta no tiene nada que ver con el contexto, simplemente di "${RESPONSE_WHEN_NO_ANSWER}". No intentes inventar una respuesta para una pregunta ramdom. 
+Usa un tono imparcial y periodístico. No repitas texto. 
+Si no encuentras la respuesta en el contexto proporcionado, puedes responder con el conocimiento que tengas, pero asegúrate de que la prioridad sea el contexto. 
 Cualquier cosa entre los siguientes bloques html context se recupera de un banco de conocimientos, no es parte de la conversación con el usuario.`;
 
 const OPENAI_MODEL = process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo-16k';
